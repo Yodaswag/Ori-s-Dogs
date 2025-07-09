084df8739e6421f625f609cac732b5e2fe96946e
Ori Shriki <49729039+Yodaswag@users.noreply.github.com>
2025-07-10 00:39:16 +0300

    Add files via upload

---
+++ Website/jScripts/JavaScript.js
@@ -0,0 +1,4 @@
+﻿function toggleExplanation(card) {
+    const explanation = card.querySelector('.explanation');
+    explanation.style.display = (explanation.style.display === 'block') ? 'none' : 'block';
+}
